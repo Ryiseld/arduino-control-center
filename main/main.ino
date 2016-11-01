c4abf2ff001afa8bf72f146add1fd5437f33cb1e
Ryiseld <ryiseld@gmail.com>
2016-11-01 17:16:57 +0200

    Add RGB LED Controls (sliders)

--- main/main.ino
+++ main/main.ino
@@ -30,6 +30,14 @@ char currentPage;
 
 int x, y;
 
+// RGB LED
+const int redLed = 10;
+const int greenLed = 9;
+const int blueLed = 8;
+int xR = 38;
+int xG = 38;
+int xB = 38;
+
 void setup()
 {
   myLCD.InitLCD();
@@ -63,6 +71,11 @@ void loop()
         if ((x >= 10) && (x <= 60) && (y >= 10) && (y <= 36)) { // Back button
           currentPage = '0';
           drawHomeScreen();
+          analogWrite(redLed, 0);
+          analogWrite(greenLed, 0);
+          analogWrite(blueLed, 0);
+        } else {
+          setLedColor();
         }
         break;
     }
@@ -102,21 +115,101 @@ void drawHomeScreen()
 
   // Made by
   myLCD.setBackColor(0, 0, 0);
-  myLCD.setColor(168, 168, 168);
+  myLCD.setColor(255, 255, 255);
   myLCD.setFont(SmallFont);
   myLCD.print("Made by Orel Lazri", 10, 220);
 }
 
-void drawLedScreen() {
+void drawLedScreen()
+{
   myLCD.clrScr();
-
-  // Back button
+  
   drawBackButton();
-
-  // Title
-  myLCD.setBackColor(0, 0, 0);
-  myLCD.setColor(255, 255, 255);
+  
   myLCD.setFont(BigFont);
-  myLCD.print("LED Control", CENTER, 50);
+  myLCD.print("RGB LED Control", CENTER, 50);
+  myLCD.print("LED Color:", 10, 95);
+  myLCD.print("R", 10, 135);
+  myLCD.print("G", 10, 175);
+  myLCD.print("B", 10, 215);
+  myLCD.setColor(255, 0, 0);
+  myLCD.drawLine(0,75,319,75); 
+  myLCD.setColor(255, 255, 255);
+  myLCD.drawRect(30, 138, 310, 148);
+  myLCD.drawRect(30, 178, 310, 188);
+  myLCD.drawRect(30, 218, 310, 228);  
 }
 
+void setLedColor()
+{
+  if (myTouch.dataAvailable()) {
+      myTouch.read();
+      x=myTouch.getX();
+      y=myTouch.getY();        
+      // Area of the Red color slider
+      if( (y>=130) && (y<=156)) {
+        xR=x; // Stores the X value where the screen has been pressed in to variable xR
+        if (xR<=38) { // Confines the area of the slider to be above 38 pixels
+          xR=38;
+        }
+        if (xR>=303){ /// Confines the area of the slider to be under 310 pixels
+          xR=303;
+        }
+      }
+      // Area of the Green color slider
+      if( (y>=170) && (y<=196)) {
+        xG=x;
+        if (xG<=38) {
+          xG=38;
+        }
+        if (xG>=303){
+          xG=303;
+        }
+      }
+      // Area of the Blue color slider
+      if( (y>=210) && (y<=236)) {
+        xB=x;
+        if (xB<=38) {
+          xB=38;
+        }
+        if (xB>=303){
+          xB=303;
+        }
+      }    
+    }
+    // Maps the values of the X - Axis from 38 to 0 and 310 to 255, because we need values from 0 to 255 for turning on the led
+    int xRC = map(xR,38,310,0,255);
+    int xGC = map(xG,38,310,0,255);
+    int xBC = map(xB,38,310,0,255);
+    
+    // Sends PWM signal to the pins of the led
+    analogWrite(redLed, xRC);
+    analogWrite(greenLed, xGC);
+    analogWrite(blueLed, xBC);
+    
+    // Draws a rectangle with the latest color combination 
+    myLCD.setColor(xRC, xGC, xBC);
+    myLCD.fillRoundRect(175, 87, 310, 119);
+    
+    // Draws the positioners
+    myLCD.setColor(255, 255, 255);
+    myLCD.fillRect(xR,139,(xR+4),147); // Positioner
+    myLCD.setColor(xRC, 0, 0);
+    myLCD.fillRect(31, 139, (xR-1), 147);
+    myLCD.setColor(0, 0, 0);
+    myLCD.fillRect((xR+5), 139, 309, 147);
+    
+    myLCD.setColor(255, 255, 255);
+    myLCD.fillRect(xG,179,(xG+4),187);
+    myLCD.setColor(0, xGC, 0);
+    myLCD.fillRect(31, 179, (xG-1), 187);
+    myLCD.setColor(0, 0, 0);
+    myLCD.fillRect((xG+5), 179, 309, 187);
+    
+    myLCD.setColor(255, 255, 255);
+    myLCD.fillRect(xB,219,(xB+4),227);
+    myLCD.setColor(0, 0, xBC);
+    myLCD.fillRect(31, 219, (xB-1), 227);
+    myLCD.setColor(0, 0, 0);
+    myLCD.fillRect((xB+5), 219, 309, 227);
+}
