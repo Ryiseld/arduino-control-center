de6254c27cc31d3a13ace5812d65fb0264fce3ba
Orel <ryiseld@gmail.com>
2017-02-21 21:52:42 +0200

    Updated to last version
    Including: RGB LED, Ultrasonic sensor (Meters and inches) and DHT11 (Temp in C,F and humidity)

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,14 @@
 #include <UTFT.h>
 #include <URTouch.h>
-//#include <UTFT_Buttons.h>
+#include <dht11.h>
 
 // LCD and everything related to it
 UTFT          myLCD(ILI9341_16, 38, 39, 40, 41);
 URTouch       myTouch(6, 5, 4, 3, 2);
-//UTFT_Buttons  myButtons(&myLCD, &myTouch);
+
+// DHT11 sensor
+#define DHT11PIN 14
+dht11 DHT11;
 
 // Fonts
 extern uint8_t SmallFont[];
@@ -19,14 +22,29 @@ extern uint8_t SevenSegNumFont[];
 #define BTN_BACK_WIDTH          60
 #define BTN_BACK_HEIGHT         36
 
+// Button parameters
+#define BTN_OFFSET 20
+#define BTN_HEIGHT 40
 
 // Led control button
 #define BTN_LED_CONTROL_X       35
 #define BTN_LED_CONTROL_Y       50
-#define BTN_LED_CONTROL_WIDTH   285
-#define BTN_LED_CONTROL_HEIGHT  90
+#define BTN_LED_CONTROL_X2      285
+#define BTN_LED_CONTROL_Y2      BTN_LED_CONTROL_Y + BTN_HEIGHT
 
-char currentPage;
+// Distance sensor button
+#define BTN_DIST_SENSOR_X       35
+#define BTN_DIST_SENSOR_Y       BTN_LED_CONTROL_Y2 + BTN_OFFSET
+#define BTN_DIST_SENSOR_X2      285
+#define BTN_DIST_SENSOR_Y2      BTN_DIST_SENSOR_Y + BTN_HEIGHT
+
+// DHT11 sensor button
+#define BTN_DHT11_SENSOR_X       35
+#define BTN_DHT11_SENSOR_Y       BTN_DIST_SENSOR_Y2 + BTN_OFFSET
+#define BTN_DHT11_SENSOR_X2      285
+#define BTN_DHT11_SENSOR_Y2      BTN_DHT11_SENSOR_Y + BTN_HEIGHT
+
+char currentPage, selectedUnit, tempUnit;
 
 int x, y;
 
@@ -38,13 +56,31 @@ int xR = 38;
 int xG = 38;
 int xB = 38;
 
+// Ultrasonic distance sensor
+const int VCC = 13;
+const int trigPin = 11;
+const int echoPin = 12;
+
+long duration;
+int distanceInch, distanceCm;
+
 void setup()
 {
+  // LCD and touch
   myLCD.InitLCD();
   myLCD.fillScr(0, 0, 0);
   
   myTouch.InitTouch();
   myTouch.setPrecision(PREC_MEDIUM);
+
+  // Defining pin modes
+  pinMode(VCC, OUTPUT);
+  digitalWrite(VCC, HIGH);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
 
   drawHomeScreen();
   currentPage = '0';
@@ -60,28 +96,74 @@ void loop()
     switch (currentPage) {
       case '0': // Home page
         // LED Control button
-        if ((x >= BTN_LED_CONTROL_X) && (x <= BTN_LED_CONTROL_WIDTH) &&
-            (y >= BTN_LED_CONTROL_Y) && (y <= BTN_LED_CONTROL_HEIGHT)) {
+        if ((x >= BTN_LED_CONTROL_X) && (x <= BTN_LED_CONTROL_X2) &&
+            (y >= BTN_LED_CONTROL_Y) && (y <= BTN_LED_CONTROL_Y2)) {
           currentPage = '1';
           drawLedScreen();
+        }
+        
+        // Distance sensor button
+        if ((x >= BTN_DIST_SENSOR_X) && (x <= BTN_DIST_SENSOR_X2) &&
+            (y >= BTN_DIST_SENSOR_Y) && (y <= BTN_DIST_SENSOR_Y2)) {
+          currentPage = '2';
+          drawDistControl();
+        }
+        
+        // DHT11 sensor button
+        if ((x >= BTN_DHT11_SENSOR_X) && (x <= BTN_DHT11_SENSOR_X2) &&
+            (y >= BTN_DHT11_SENSOR_Y) && (y <= BTN_DHT11_SENSOR_Y2)) {
+          currentPage = '3';
+          drawDhtControl();
         }
         break;
 
       case '1': // LED Control
-        if ((x >= 10) && (x <= 60) && (y >= 10) && (y <= 36)) { // Back button
-          currentPage = '0';
-          drawHomeScreen();
-          analogWrite(redLed, 0);
-          analogWrite(greenLed, 0);
-          analogWrite(blueLed, 0);
-        } else {
-          setLedColor();
+          if (!detectBackButton()) {
+            setLedColor();
+          } else {
+            analogWrite(redLed, 0);
+            analogWrite(greenLed, 0);
+            analogWrite(blueLed, 0); 
+          } 
+        break;
+        
+      case '2': // Distance sensor
+        if (!detectBackButton()) {
+          showDistance();
+           
+          // If we press the Centimeters Button
+          if ((x>=10) && (x<=135) &&(y>=90) && (y<=163)) {
+            selectedUnit = '0';
+          }
+          
+          // If we press the Inches Button
+          if ((x>=10) && (x<=135) &&(y>=173) && (y<=201)) {
+            selectedUnit = '1';
+          }
         }
+        break;
+        
+      case '3': // DHT11 sensor
+        if (!detectBackButton()) {
+          showDht(); 
+          
+          // If we press the Celcius Button
+          if ((x>=10) && (x<=135) &&(y>=90) && (y<=163)) {
+            tempUnit = '0';
+          }
+          
+          // If we press the Fahrenheit Button
+          if ((x>=10) && (x<=135) &&(y>=173) && (y<=201)) {
+            tempUnit = '1';
+          }
+        }
+        
         break;
     }
   }
 }
 
+// Screen functions
 void drawBackButton() {
   myLCD.setColor(100, 155, 203);
   myLCD.fillRoundRect (BTN_BACK_X, BTN_BACK_Y, BTN_BACK_WIDTH, BTN_BACK_HEIGHT);
@@ -107,11 +189,27 @@ void drawHomeScreen()
 
   // LED control button
   myLCD.setColor(16, 167, 103);
-  myLCD.fillRoundRect(BTN_LED_CONTROL_X, BTN_LED_CONTROL_Y, BTN_LED_CONTROL_WIDTH, BTN_LED_CONTROL_HEIGHT);
+  myLCD.fillRoundRect(BTN_LED_CONTROL_X, BTN_LED_CONTROL_Y, BTN_LED_CONTROL_X2, BTN_LED_CONTROL_Y2);
   myLCD.setColor(255, 255, 255);
-  myLCD.drawRoundRect(BTN_LED_CONTROL_X, BTN_LED_CONTROL_Y, BTN_LED_CONTROL_WIDTH, BTN_LED_CONTROL_HEIGHT);
+  myLCD.drawRoundRect(BTN_LED_CONTROL_X, BTN_LED_CONTROL_Y, BTN_LED_CONTROL_X2, BTN_LED_CONTROL_Y2);
   myLCD.setBackColor(16, 167, 103);
-  myLCD.print("LED Control", CENTER, ((BTN_LED_CONTROL_Y + BTN_LED_CONTROL_HEIGHT) / 2) - 5);
+  myLCD.print("LED Control", CENTER, ((BTN_LED_CONTROL_Y + BTN_LED_CONTROL_Y2) / 2) - 5);
+  
+  // Distance sensor button
+  myLCD.setColor(16, 167, 103);
+  myLCD.fillRoundRect(BTN_DIST_SENSOR_X, BTN_DIST_SENSOR_Y, BTN_DIST_SENSOR_X2, BTN_DIST_SENSOR_Y2);
+  myLCD.setColor(255, 255, 255);
+  myLCD.drawRoundRect(BTN_DIST_SENSOR_X, BTN_DIST_SENSOR_Y, BTN_DIST_SENSOR_X2, BTN_DIST_SENSOR_Y2);
+  myLCD.setBackColor(16, 167, 103);
+  myLCD.print("Distance Sensor", CENTER, ((BTN_DIST_SENSOR_Y + BTN_DIST_SENSOR_Y2) / 2) - 5);
+  
+  // DHT11 sensor button
+  myLCD.setColor(16, 167, 103);
+  myLCD.fillRoundRect(BTN_DHT11_SENSOR_X, BTN_DHT11_SENSOR_Y, BTN_DHT11_SENSOR_X2, BTN_DHT11_SENSOR_Y2);
+  myLCD.setColor(255, 255, 255);
+  myLCD.drawRoundRect(BTN_DHT11_SENSOR_X, BTN_DHT11_SENSOR_Y, BTN_DHT11_SENSOR_X2, BTN_DHT11_SENSOR_Y2);
+  myLCD.setBackColor(16, 167, 103);
+  myLCD.print("DHT11 Sensor", CENTER, ((BTN_DHT11_SENSOR_Y + BTN_DHT11_SENSOR_Y2) / 2) - 5);
 
   // Made by
   myLCD.setBackColor(0, 0, 0);
@@ -123,7 +221,6 @@ void drawHomeScreen()
 void drawLedScreen()
 {
   myLCD.clrScr();
-  
   drawBackButton();
   
   myLCD.setFont(BigFont);
@@ -138,6 +235,80 @@ void drawLedScreen()
   myLCD.drawRect(30, 138, 310, 148);
   myLCD.drawRect(30, 178, 310, 188);
   myLCD.drawRect(30, 218, 310, 228);  
+}
+
+void drawDistControl()
+{
+  myLCD.clrScr();
+  drawBackButton();
+  
+  myLCD.print("Ultrasonic Sensor", CENTER, 50);
+  myLCD.print("Press a unit to see distance", CENTER, 76);
+  myLCD.setColor(255, 0, 0);
+  myLCD.drawLine(0,100,319,100);
+  myLCD.setBackColor(0, 0, 0);
+  myLCD.setColor(255, 255, 255);
+  myLCD.setFont(SmallFont);
+  myLCD.print("Select Unit", 10, 114);
+  myLCD.setFont(BigFont);
+  myLCD.print("Distance:", 130, 120);
+  myLCD.setColor(223, 77, 55);
+  myLCD.fillRoundRect (10, 135, 90, 163);
+  myLCD.setColor(225, 255, 255);
+  myLCD.drawRoundRect (10, 135, 90, 163);
+  myLCD.setBackColor(223, 77, 55);
+  myLCD.setColor(255, 255, 255);
+  myLCD.print("cm", 33, 140);
+  myLCD.setColor(223, 77, 55);
+  myLCD.fillRoundRect (10, 173, 90, 201);
+  myLCD.setColor(255, 255, 255);
+  myLCD.drawRoundRect (10, 173, 90, 201);
+  myLCD.setBackColor(223, 77, 55);
+  myLCD.setColor(255, 255, 255);
+  myLCD.print("inch", 17, 180);
+}
+
+void drawDhtControl()
+{
+  myLCD.clrScr();
+  drawBackButton();
+ 
+  myLCD.print("DHT11 Sensor", CENTER, 50);
+  myLCD.print("Temperature and Humidity", CENTER, 76);
+  myLCD.setColor(255, 0, 0);
+  myLCD.drawLine(0,100,319,100);
+  myLCD.setBackColor(0, 0, 0);
+  myLCD.setColor(255, 255, 255);
+  myLCD.setFont(SmallFont);
+  myLCD.print("Select Unit", 10, 114);
+  myLCD.setFont(BigFont);
+  myLCD.print("Temperature:", 130, 120);
+  myLCD.setColor(223, 77, 55);
+  myLCD.fillRoundRect (10, 135, 90, 163);
+  myLCD.setColor(225, 255, 255);
+  myLCD.drawRoundRect (10, 135, 90, 163);
+  myLCD.setBackColor(223, 77, 55);
+  myLCD.setColor(255, 255, 255);
+  myLCD.print("C", 40, 140);
+  myLCD.setColor(223, 77, 55);
+  myLCD.fillRoundRect (10, 173, 90, 201);
+  myLCD.setColor(255, 255, 255);
+  myLCD.drawRoundRect (10, 173, 90, 201);
+  myLCD.setBackColor(223, 77, 55);
+  myLCD.setColor(255, 255, 255);
+  myLCD.print("F", 40, 180);
+}
+
+// Utility functions
+boolean detectBackButton()
+{
+  if ((x >= 10) && (x <= 60) && (y >= 10) && (y <= 36)) { // Back button
+    currentPage = '0';
+    drawHomeScreen();
+    return true;
+  }
+  
+  return false;
 }
 
 void setLedColor()
@@ -212,4 +383,78 @@ void setLedColor()
     myLCD.fillRect(31, 219, (xB-1), 227);
     myLCD.setColor(0, 0, 0);
     myLCD.fillRect((xB+5), 219, 309, 227);
+}
+
+void showDistance()
+{
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin to HIGH for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * 0.034 / 2;
+  distanceInch = distanceCm / 2.53;
+  
+  // Print the distance in centimeters
+  if (selectedUnit == '0' && distanceCm <= 400) {
+    myLCD.setFont(SevenSegNumFont);
+    myLCD.setColor(0, 255, 0);
+    myLCD.setBackColor(0, 0, 0);
+    myLCD.printNumI(distanceCm,130, 145, 3,'0');
+    myLCD.setFont(BigFont);
+    myLCD.print("cm  ", 235, 178);
+  
+  }
+  
+  // Print the distance in inches
+  if (selectedUnit == '1' && distanceCm <= 160) {
+    myLCD.setFont(SevenSegNumFont);
+    myLCD.setColor(0, 255, 0);
+    myLCD.setBackColor(0, 0, 0);
+    myLCD.printNumI(distanceInch,130, 145, 3,'0');
+    myLCD.setFont(BigFont);
+    myLCD.print("inch", 235, 178);
+  } 
+  
+  delay(1);
+}
+
+void showDht()
+{
+  int chk = DHT11.read(DHT11PIN);
+ 
+  // Print the temperature in Celcius
+  if (tempUnit == '0') {
+    myLCD.setFont(SevenSegNumFont);
+    myLCD.setColor(0, 255, 0);
+    myLCD.setBackColor(0, 0, 0);
+    myLCD.printNumI((int)DHT11.temperature,130, 145, 3,'0');
+    myLCD.setFont(BigFont);
+    myLCD.print("C", 235, 178);
+  
+  }
+  
+  // Print the temperature in Fahrenheit
+  if (tempUnit == '1') {
+    myLCD.setFont(SevenSegNumFont);
+    myLCD.setColor(0, 255, 0);
+    myLCD.setBackColor(0, 0, 0);
+    myLCD.printNumI((1.8 * (int)DHT11.temperature) + 32,130, 145, 3,'0');
+    myLCD.setFont(BigFont);
+    myLCD.print("F", 235, 178);
+  } 
+  
+  // Print humidity
+  myLCD.setFont(BigFont);
+  myLCD.setColor(255, 255, 255);
+  myLCD.print("% Humidity: ", 80, 220);
+  myLCD.printNumI((int)DHT11.humidity, 260, 220, 2, '0');
 }
